431523526779ef6199317c5f3e22ab89abe4279d
AymenDaoudi <aymendaoudi@outlook.com>
2015-03-25 10:11:26 +0100

    First Commit

---
+++ UsefulConverters/AddSenderToEventArgsParameterConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UsefulConverters
+{
+    public class AddSenderToEventArgsParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Tuple<Object, EventArgs>(parameter, (EventArgs)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (EventArgs)((Tuple<Object, EventArgs>)value).Item2;
+        }
+    }
+}
